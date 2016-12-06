5c2dff2bb17439246d3d6c643ef04f5d888038e4
Haran Jackson <jackson.haran@gmail.com>
2016-12-06 01:12:04 +0000

    julia weno implementation - has bugs

---
+++ julia/weno.jl
@@ -0,0 +1,147 @@
+# Implements the WENO method used in Dumbser et al (DOI 10.1016/j.cma.2013.09.022)
+include("weno_matrices.jl")
+
+function extend(inarray, nx, ny, nz, nvar, d)
+  # Extends the input array by M cells on each surface
+  if d==1
+    ret = zeros(nx+2N, ny, nz, nvar)
+    ret[N+1:nx+N, :, :, :] = inarray
+    for i = 1:N
+      ret[i, :, :, :] = inarray[1, :, :, :]
+      ret[nx+N+i, :, :, :] = inarray[nx, :, :, :]
+    end
+    return ret
+
+  elseif d==2
+    ret = zeros(nx, ny+2N, nz, nvar)
+    ret[:, N+1:ny+N, :, :] = inarray
+    for i = 1:N
+      ret[:, i, :, :] = inarray[:, 1, :, :]
+      ret[:, ny+N+i, :, :] = inarray[:, ny, :, :]
+    end
+    return ret
+
+  else
+    ret = zeros(nx, ny, nz+2N, nvar)
+    ret[:, :, N+1:nz+N, :] = inarray
+    for i = 1:N
+      ret[:, :, i, :] = inarray[:, :, 1, :]
+      ret[:, :, nz+N+i, :] = inarray[:, :, nz, :]
+    end
+    return ret
+  end
+end
+
+function coeffs(w0list, N, nvar, n, Mlist, λlist, Σ, ε, r)
+  # Calculate coefficients of basis polynomials and weights
+  wlist = [Mlist[i] \ w0list[i] for i = 1:n]
+  σlist = [diag(w' * Σ * w) for w in wlist]
+  olist = [λlist[i]  ./ (abs(σlist[i]) + ε).^r for i = 1:n]
+  oSum = zeros(nvar)
+  num = zeros(N+1, nvar)
+  print(n, size(num)," ",size(wlist[1])," ",size(olist[1]))
+  for i = 1:n
+    oSum += olist[i]
+    for j = 1:N+1
+      num[j] += wlist[i][j] .* olist[i]
+    end
+  end
+  return num ./ oSum
+end
+
+function weno(u, N, λc=1e5, λs=1, r=8, ε=1e-14)
+  # Find reconstruction coefficients of u to order N+1
+  nx, ny, nz, nvar = size(u)
+  fHalfN = Int(floor(N/2))
+  cHalfN = Int(ceil(N/2))
+
+  if Bool(N%2)
+    n = 4
+    λlist = [λc, λc, λs, λs]
+  else
+    n = 3
+    λlist = [λc, λs, λs]
+  end
+
+  Mlist = coefficient_matrices(N, n)
+  Σ = oscillation_indicator(N)
+
+  Wx = zeros(nx, ny, nz, N+1, nvar)
+  Wx0 = extend(u, nx, ny, nz, nvar, 1)
+  for i = 1:nx
+    for j = 1:ny
+      for k = 1:nz
+        ii = i + N
+        if n==3
+          w1 = Wx0[ii-fHalfN : ii+fHalfN, j, k, :]
+          w2 = Wx0[ii-N : ii,             j, k, :]
+          w3 = Wx0[ii : ii+N,             j, k, :]
+          Wx[i, j, k, :, :] = coeffs([w1, w2, w3], N, nvar, n, Mlist, λlist, Σ, ε, r)
+        else
+          w1 = Wx0[ii-fHalfN : ii+cHalfN, j, k, :]
+          w2 = Wx0[ii-cHalfN : ii+fHalfN, j, k, :]
+          w3 = Wx0[ii-N : ii,             j, k, :]
+          w4 = Wx0[ii : ii+N,             j, k, :]
+          Wx[i, j, k, :, :] = coeffs([w1, w2, w3, w4], N, nvar, n, Mlist, λlist, Σ, ε, r)
+        end
+      end
+    end
+  end
+  if ny==1 && nz==1
+    return Wx
+  end
+
+  Wxy = zeros(nx, ny, nz, N+1, N+1, nvar)
+  tempWx = extend(Wx, nx, ny, nz, nvar, 2)
+  for i = 1:nx
+    for j = 1:ny
+      for k = 1:nz
+        jj = j + N
+        for a = 1:N+1
+          if n==3
+            w1 = Wxy0[i, jj-fHalfN : jj+fHalfN, k, a, :]
+            w2 = Wxy0[i, jj-N : jj,             k, a, :]
+            w3 = Wxy0[i, jj : jj+N,             k, a, :]
+            Wxy[i, j, k, a, :, :] = coeffs([w1, w2, w3], N, nvar, n, Mlist, λlist, Σ, ε, r)
+          else
+            w1 = Wxy0[i, jj-fHalfN : jj+cHalfN, k, a, :]
+            w2 = Wxy0[i, jj-cHalfN : jj+fHalfN, k, a, :]
+            w3 = Wxy0[i, jj-N : jj,             k, a, :]
+            w4 = Wxy0[i, jj : jj+N,             k, a, :]
+            Wxy[i, j, k, a, :, :] = coeffs([w1, w2, w3, w4], N, nvar, n, Mlist, λlist, Σ, ε, r)
+          end
+        end
+      end
+    end
+  end
+  if nz==1
+    return Wxy
+  end
+
+  Wxyz = zeros(nx, ny, nz, N+1, N+1, N+1, nvar)
+  Wxyz0 = extend(Wxy, nx, ny, nz, nvar, 3)
+  for i in 1:nx
+    for j = 1:ny
+      for k = 1:nz
+        kk = k + N
+        for a = 1:N+1
+          for b = 1:N+1
+            if n==3
+              w1 = Wxyz0[i, j, kk-fHalfN : kk+fHalfN, a, b, :]
+              w2 = Wxyz0[i, j, kk-N : kk,             a, b, :]
+              w3 = Wxyz0[i, j, kk : kk+N,             a, b, :]
+              Wxyz[i, j, k, a, b, :, :] = coeffs([w1, w2, w3], N, nvar, n, Mlist, λlist, Σ, ε, r)
+            else
+              w1 = Wxyz0[i, j, kk-fHalfN : kk+cHalfN, a, b, :]
+              w2 = Wxyz0[i, j, kk-cHalfN : kk+fHalfN, a, b, :]
+              w3 = Wxyz0[i, j, kk-N : kk,             a, b, :]
+              w4 = Wxyz0[i, j, kk : kk+N,             a, b, :]
+              Wxyz[i, j, k, a, b, :, :] = coeffs([w1, w2, w3, w4], N, nvar, n, Mlist, λlist, Σ, ε, r)
+            end
+          end
+        end
+      end
+    end
+  end
+  return Wxyz
+end
---
+++ julia/weno_matrices.jl
@@ -0,0 +1,107 @@
+import Polynomials: Poly, polyint, polyder
+
+function lagrange(x, w)
+  """
+  Taken from SciPy 0.18.1:
+
+  Return a Lagrange interpolating polynomial.
+  Given two 1-D arrays `x` and `w,` returns the Lagrange interpolating
+  polynomial through the points ``(x, w)``.
+  Warning: This implementation is numerically unstable. Do not expect to
+  be able to use more than about 20 points even if they are chosen optimally.
+  Parameters
+  ----------
+  x : array_like
+      `x` represents the x-coordinates of a set of datapoints.
+  w : array_like
+      `w` represents the y-coordinates of a set of datapoints, i.e. f(`x`).
+  Returns
+  -------
+      The Lagrange interpolating polynomial.
+  """
+  M = size(x)[1]
+  p = Poly([0])
+  for j in 1:M
+    pt = Poly([w[j]])
+    for k in 1:M
+      if k != j
+        fac = x[j]-x[k]
+        pt *= Poly([1.0, -x[k]]) / fac
+      end
+    end
+    p += pt
+  end
+  return p
+end
+
+function nodes(N)
+  # Returns Legendre-Gauss nodes, scaled to [0,1]
+  if N==1
+    return [0.5]
+  elseif N==2
+    return 0.5 * (1 + [-sqrt(1/3), sqrt(1/3)])
+  elseif N==3
+    tmp = sqrt(3/5)
+    return 0.5 * (1 + [-tmp, 0, tmp])
+  elseif N==4
+    tmp = 2/7 * sqrt(6/5)
+    tmp1 = sqrt(3/7 - tmp)
+    tmp2 = sqrt(3/7 + tmp)
+    return 0.5 * (1 + [-tmp2, -tmp1, tmp1, tmp2])
+  elseif N==5
+    tmp = 2 * sqrt(10/7)
+    tmp1 = 1/3 * sqrt(5-tmp)
+    tmp2 = 1/3 * sqrt(5+tmp)
+    return 0.5 * (1 + [-tmp2, -tmp1, 0, tmp1, tmp2])
+  else
+    print("Invalid input")
+  end
+end
+
+function basis(N)
+  # Returns basis polynomials
+  nodeArray = nodes(N+1)
+  return [lagrange(nodeArray, eye(N+1)[:,i]) for i in 1:N+1]
+end
+
+function coefficient_matrices(N, n)
+  # Generate linear systems governing the coefficients of the basis polynomials
+  fHalfN = floor(N/2)
+  cHalfN = ceil(N/2)
+  Mlist = zeros(n, N+1, N+1)
+  ψ = basis(N)
+
+  for a in 1:N+1
+    for p in 1:N+1
+      ψintp = polyint(ψ[p])
+      if Bool(N%2)
+        Mlist[1,a,p] = ψintp(a-fHalfN) - ψintp(a-fHalfN-1)
+        Mlist[2,a,p] = ψintp(a-cHalfN) - ψintp(a-cHalfN-1)
+        Mlist[3,a,p] = ψintp(a-N)      - ψintp(a-N-1)
+        Mlist[4,a,p] = ψintp(a)        - ψintp(a-1)
+      else
+        Mlist[1,a,p] = ψintp(a-fHalfN) - ψintp(a-fHalfN-1)
+        Mlist[2,a,p] = ψintp(a-N)      - ψintp(a-N-1)
+        Mlist[3,a,p] = ψintp(a)        - ψintp(a-1)
+      end
+    end
+  end
+  return Mlist
+end
+
+function oscillation_indicator(N)
+  # Generate the oscillation indicator matrix
+  Σ = zeros(N+1, N+1)
+  ψ = basis(N)
+
+  for a in 1:N
+    ψdera = [polyder(ψp, a) for ψp in ψ]
+    for p in 1:N+1
+      for m in 1:N+1
+        antiderivative = polyint(ψdera[p] * ψdera[m])
+        Σ[p,m] += antiderivative(1) - antiderivative(0)
+      end
+    end
+  end
+  return Σ
+end
