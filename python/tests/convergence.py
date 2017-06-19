f1494960d3abfb4d39cc8348935aaf35c07e0e6c
Haran Jackson <jackson.haran@gmail.com>
2017-06-20 00:55:17 +0100

    made convergence test 3d

--- python/tests/convergence.py
+++ python/tests/convergence.py
@@ -1,9 +1,16 @@
 from numpy import amax, inf, log, sqrt, sum, zeros
 
 
-def convergence_rate(exact1, exact2, results1, results2, n1, n2, L=2, var=0):
-    diff1 = (results1 - exact1)[:,0,0,var]
-    diff2 = (results2 - exact2)[:,0,0,var]
+def convergence_rate(exact1, exact2, results1, results2, L=2, var=0):
+
+    diff1 = (results1 - exact1)[:,:,:,var]
+    diff2 = (results2 - exact2)[:,:,:,var]
+
+    nx1,ny1,nz1 = diff1.shape[:3]
+    nx2,ny2,nz2 = diff2.shape[:3]
+
+    n1 = nx1*ny1*nz1
+    n2 = nx2*ny2*nz2
 
     if L==1:
         ε1 = sum(abs(diff1)) / n1
@@ -15,9 +22,9 @@ def convergence_rate(exact1, exact2, results1, results2, n1, n2, L=2, var=0):
         ε1 = amax(abs(diff1))
         ε2 = amax(abs(diff2))
 
-    print(ε1/n1)
-    print(ε2/n2)
-    return log((ε2/n2)/(ε1/n1)) / log(n1/n2)
+    print(ε1)
+    print(ε2)
+    return log((ε2)/(ε1)) / log(nx1/nx2)
 
 def resize_array(arr,n):
     # reduces size of arr along dimension 0 to length n
