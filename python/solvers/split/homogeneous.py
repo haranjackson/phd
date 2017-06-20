5d58f3d0005f6b9b87f3b24156d659a1785b82b3
Haran Jackson <jackson.haran@gmail.com>
2017-06-20 22:32:32 +0100

    made weno midstepper 2d

--- python/solvers/split/homogeneous.py
+++ python/solvers/split/homogeneous.py
@@ -1,31 +1,70 @@
-from numpy import dot, zeros
+from itertools import product
 
-from options import dx, N1, nx
+from numpy import dot, tensordot, zeros
+
+from options import dx, dy, N1, nx, ny, nz, ndim
 from solvers.basis import derivative_values
 from gpr.matrices.conserved import B0dot, flux, system_conserved
 
 
 derivs = derivative_values()
 
+def derivative(X, dim):
+    """ Returns the derivative of polynomial coefficients X with respect to
+        dimension dim. X can be of shape (N1,...) or (N1,N1,...)
+    """
+    if dim==0:
+        return tensordot(derivs, X, (1,0)) / dx
+    elif dim==1:
+        return tensordot(derivs, X, (1,1)).swapaxes(0,1) / dy
+
 def weno_midstepper(wh, dt, PAR, SYS):
     """ Steps the WENO reconstruction forwards by dt/2, under the homogeneous system
     """
     USE_JACOBIAN = 0
 
-    F = zeros([N1, 18])
+    F = zeros([N1]*ndim+[18])
+    G = zeros([N1]*ndim+[18])
     Bdwdx = zeros(18)
-    for i in range(nx):
-        w = wh[i,0,0]
-        dwdx = dot(derivs, w)
+    Bdwdy = zeros(18)
 
-        if USE_JACOBIAN:
-            for j in range(N1):
-                w[j] -= dt/2 * dot(system_conserved(w[j],0,PAR,SYS), dwdx[j]/dx)
-        else:
-            for j in range(N1):
-                F[j] = flux(w[j], 0, PAR, SYS)
-            dFdx = dot(derivs, F)
-            for j in range(N1):
-                v = w[j,2:5] / w[j,0]
-                B0dot(Bdwdx, dwdx[j], v)
-                w[j] -= dt/(2*dx) * (dFdx[j] + Bdwdx)
+    for i,j,k in product(range(nx), range(ny), range(nz)):
+
+        w = wh[i,j,k]
+        dwdx = derivative(w, 0)
+
+        if ndim==1:
+
+            if USE_JACOBIAN:
+                for a in range(N1):
+                    Mx = system_conserved(w[a], 0, PAR, SYS)
+                    w[a] -= dt/2 * dot(Mx, dwdx[a])
+            else:
+                for a in range(N1):
+                    F[a] = flux(w[a], 0, PAR, SYS)
+                dFdx = dot(derivs, F)
+                for a in range(N1):
+                    v = w[a,2:5] / w[a,0]
+                    B0dot(Bdwdx, dwdx[a], v)
+                    w[a] -= dt/2 * (dFdx[a] + Bdwdx)
+
+        elif ndim==2:
+
+            dwdy = derivative(w, 1)
+
+            if USE_JACOBIAN:
+                for a,b in product(range(N1), range(N1)):
+                    Mx = system_conserved(w[a,b], 0, PAR, SYS)
+                    My = system_conserved(w[a,b], 1, PAR, SYS)
+                    w[a,b] -= dt/2 * (dot(Mx, dwdx[a,b]) + dot(My, dwdy[a,b]))
+            else:
+                for a,b in product(range(N1), range(N1)):
+                    F[a,b] = flux(w[a,b], 0, PAR, SYS)
+                    G[a,b] = flux(w[a,b], 1, PAR, SYS)
+                dFdx = derivative(F, 0)
+                dGdy = derivative(G, 1)
+                for a,b in product(range(N1), range(N1)):
+                    v = w[a,b,2:5] / w[a,b,0]
+                    B0dot(Bdwdx, dwdx[a,b], v)
+                    B0dot(Bdwdy, dwdy[a,b], v)
+                    w[a,b] -= dt/2 * (dFdx[a,b] + dGdy[a,b] + Bdwdx + Bdwdy)
