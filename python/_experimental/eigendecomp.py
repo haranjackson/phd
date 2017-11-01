666829668a619c3777fde4470762fd4a28c2d8ef
Haran Jackson <jackson.haran@gmail.com>
2017-11-01 15:40:18 +0000

    organised files

--- python/_experimental/eigendecomp.py
+++ python/_experimental/eigendecomp.py
@@ -115,10 +115,24 @@ def solver_vectors(A0, dt):
 ### Angles Formulation ###
 
 def rotmat_angles(x,y,z):
-    Rx = array([[1,0,0],[0,cos(x),-sin(x)],[0,sin(x),cos(x)]])
-    Ry = array([[cos(y),0,sin(y)],[0,1,0],[-sin(y),0,cos(y)]])
-    Rz = array([[cos(z),-sin(z),0],[sin(z),cos(z),0],[0,0,1]])
-    return dot(Rz,dot(Ry,Rx))
+    cx = cos(x)
+    sx = sin(x)
+    cy = cos(y)
+    sy = sin(y)
+    cz = cos(z)
+    sz = sin(z)
+
+    ret = zeros([3,3])
+    ret[0,0] = cy*cz
+    ret[0,1] = sx*sy*cz - cx*sz
+    ret[0,2] = cx*sy*cz + sx*sz
+    ret[1,0] = cy*sz
+    ret[1,1] = sx*sy*sz + cx*cz
+    ret[1,2] = cx*sy*sz - sx*cz
+    ret[2,0] = -sy
+    ret[2,1] = sx*cy
+    ret[2,2] = cx*cy
+    return ret
 
 def Minv(x,y,z,λ1,λ2,λ3):
     return array([[cos(x)*tan(y)/lim(λ1-λ2), -sin(x)*tan(y)/lim(λ1-λ3), 1/lim(λ2-λ3)],
