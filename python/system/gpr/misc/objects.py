d4d67a32979c964a0207a1d6daf9dcc3f18aa052
Haran Jackson <jackson.haran@gmail.com>
2018-01-09 17:05:18 +0000

    bugfix

--- python/system/gpr/misc/objects.py
+++ python/system/gpr/misc/objects.py
@@ -15,13 +15,14 @@ class hyperelastic_params():
         self.B0 = b0**2
 
 class EOS_params():
-    def __init__(self, EOS, ρ0, cv, p0, α, β, γ, pINF, c0, Γ0, s, A, B, R1, R2):
+    def __init__(self, EOS, ρ0, cv, p0, Tref, α, β, γ, pINF, c0, Γ0, s, A, B, R1, R2):
 
         self.EOS = eos_text_to_code(EOS)
 
         self.ρ0 = ρ0
         self.cv = cv
         self.p0 = p0
+        self.Tref = Tref
 
         if EOS == 'sg':
             self.γ = γ
@@ -48,7 +49,7 @@ def params(MP, Rc, EOS, ρ0, p0, Tref, T0, cv,
            α, β, γ, pINF,
            c0, Γ0, s, e0,
            A, B, R1, R2,
-           b0, β, τ1, μ, σY, n, PLASTIC,
+           b0, τ1, μ, σY, n, PLASTIC,
            cα, τ2,
            REACTION, Qc,
            Kc, Ti,
@@ -132,7 +133,7 @@ def material_parameters(EOS, ρ0, cv, p0,
                         Tref=None, α=None, β=None, γ=None, pINF=None,
                         c0=None, Γ0=None, s=None, e0=None,
                         A=None, B=None, R1=None, R2=None,
-                        b0=None, β=None, μ=None, τ1=None,
+                        b0=None, μ=None, τ1=None,
                         σY=None, n=None, PLASTIC=False,
                         cα=None, κ=None, Pr=None,
                         REACTION=None, Qc=None,
@@ -154,7 +155,7 @@ def material_parameters(EOS, ρ0, cv, p0,
     if (γ is not None) and (pINF is None):
         pINF = 0
 
-    P = EOS_params(EOS, ρ0, cv, p0, α, β, γ, pINF, c0, Γ0, s, A, B, R1, R2)
+    P = EOS_params(EOS, ρ0, cv, p0, Tref, α, β, γ, pINF, c0, Γ0, s, A, B, R1, R2)
     T0 = temperature(ρ0, p0, P)
 
     if b0 is not None:
@@ -180,7 +181,7 @@ def material_parameters(EOS, ρ0, cv, p0,
            α, β, γ, pINF,
            c0, Γ0, s, e0,
            A, B, R1, R2,
-           b0, β, τ1, μ, σY, n, PLASTIC,
+           b0, τ1, μ, σY, n, PLASTIC,
            cα, τ2,
            REACTION, Qc,
            Kc, Ti,
