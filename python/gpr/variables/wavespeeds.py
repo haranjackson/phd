40c54a0434314a1819da9c01fad7ce648d78fa01
Haran Jackson <jackson.haran@gmail.com>
2018-01-20 13:56:36 +0100

    bugfix

--- python/gpr/variables/wavespeeds.py
+++ python/gpr/variables/wavespeeds.py
@@ -1,6 +1,7 @@
 from numpy import sqrt
 
-from gpr.variables.mg import Γ_MG, dedρ, dedp
+from gpr.variables.eos import dEdρ, dEdp
+from gpr.variables.mg import Γ_MG, dΓ_MG
 
 
 def c_s2(ρ, MP):
@@ -32,16 +33,18 @@ def dC_0dρ(ρ, MP):
     """ Returns the derivative of C0 with respect to ρ
     """
     β = MP.β
+    cs2 = c_s2(ρ, MP)
     dcs2dρ = dc_s2dρ(ρ, MP)
     Γ = Γ_MG(ρ, MP)
-    return (1 - β/Γ) * dcs2dρ
+    dΓdρ = dΓ_MG(ρ, MP)
+    return (1 - β/Γ) * dcs2dρ + β/Γ**2 * dΓdρ * cs2
 
 def c_0(ρ, p, MP):
     """ Returns the adiabatic sound speed for the Mie-Gruneisen EOS
     """
-    de_dρ = dedρ(ρ, p, MP)
-    de_dp = dedp(ρ, MP)
-    return sqrt((p / ρ**2 - de_dρ) / de_dp)
+    dE_dρ = dEdρ(ρ, p, MP)
+    dE_dp = dEdp(ρ, MP)
+    return sqrt((p / ρ**2 - dE_dρ) / dE_dp)
 
 def c_inf(ρ, p, MP):
     """ Returns the longitudinal characteristic speed
