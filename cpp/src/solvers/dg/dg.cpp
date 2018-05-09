7ce561d855954b02c8af33ea5b38b47d10d50c9b
Haran Jackson <jackson.haran@gmail.com>
2018-05-09 20:06:49 +0100

    refactored dg

--- cpp/src/solvers/dg/dg.cpp
+++ cpp/src/solvers/dg/dg.cpp
@@ -4,6 +4,7 @@
 #include "../../system/equations.h"
 #include "../../system/objects/gpr_objects.h"
 #include "../evaluations.h"
+#include "initial_guess.h"
 
 MatN2_V rhs1(MatN2_Vr q, MatN2_Vr Ww, double dt, double dx, Par &MP) {
 
@@ -121,50 +122,6 @@ Vec obj2(Vecr q, MatN3_Vr Ww, double dt, double dx, double dy, Par &MP) {
         }
   VecMap ret(tmp.data(), N * N * N * V);
   return ret;
-}
-
-void standard_initial_guess(Matr q, Matr w, int NT) {
-  // Returns a Galerkin intial guess consisting of the value of q at t=0
-  for (int i = 0; i < N; i++)
-    q.block(i * NT, 0, NT, V) = w;
-}
-
-void hidalgo_initial_guess(Matr q, Matr w, int NT, double dt, Par &MP) {
-  // Returns the initial guess found in DOI: 10.1007/s10915-010-9426-6
-
-  standard_initial_guess(q, w, NT);
-  /*
-  Mat qt = w;
-
-  for (int t=0; t<N; t++)
-  {
-      double DT;
-      if (t==0)
-          Dt = dt * NODES(0);
-      else
-          DT = dt * (NODES(t) - NODES(t-1));
-
-      Mat dqdxt = derivs.dot(qt);
-
-      for (int i=0; i<N; i++)
-      {
-          M = system_conserved(qi, 0, PAR, SYS).dot(dqdxt[i]);
-          S = source(qt[i], PAR, SYS);
-
-          if (superStiff)
-          {
-              f = lambda X: X - qt[i] + dt/dx * M - dt/2 *
-  (S+source(X,PAR,SYS));
-              q[t,i] = newton_krylov(f, qi, f_tol=TOL);
-          }
-          else
-          {
-              q[t,i] = qi - dt/dx * M + dt * Sj;
-          }
-      }
-      qt = q[t];
-  }
-  */
 }
 
 void initial_condition(Matr Ww, Matr w, int ndim) {
---
+++ cpp/src/solvers/dg/initial_guess.h
@@ -0,0 +1,11 @@
+#ifndef INITIAL_GUESS_H
+#define INITIAL_GUESS_H
+
+#include "../../etc/types.h"
+#include "../../system/objects/gpr_objects.h"
+
+void standard_initial_guess(Matr q, Matr w, int NT);
+
+void hidalgo_initial_guess(Matr q, Matr w, int NT, double dt, Par &MP);
+
+#endif // INITIAL_GUESS_H
