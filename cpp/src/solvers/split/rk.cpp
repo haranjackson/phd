5b9f5a06a56c974214481dfca3247bb39e29a996
Haran Jackson <jackson.haran@gmail.com>
2018-12-14 08:22:47 +0000

    fixed warnings

--- cpp/src/solvers/split/rk.cpp
+++ cpp/src/solvers/split/rk.cpp
@@ -9,7 +9,7 @@ auto rk4(std::function<double(double)> f)
         return [y, dt, f](double dy1) -> double {
             return [y, dt, f, dy1](double dy2) -> double {
                 return [y, dt, f, dy1, dy2](double dy3) -> double {
-                    return [y, dt, f, dy1, dy2, dy3](double dy4) -> double {
+                    return [f, dy1, dy2, dy3](double dy4) -> double {
                         return (dy1 + 2 * dy2 + 2 * dy3 + dy4) / 6;
                     }(dt * f(y + dy3));
                 }(dt * f(y + dy2 / 2));
@@ -22,8 +22,6 @@ double runge_kutta(std::function<double(double)> f, double tf, double y,
                    int N_STEP)
 {
     auto dy = rk4(f);
-
-    double t = 0.;
 
     double dt = tf / N_STEP;
 
@@ -39,7 +37,7 @@ double runge_kutta_launcher(std::function<double(double)> f, double tf,
 
     double y0 = runge_kutta(f, tf, y, N_STEP);
 
-    while (isnan(y0))
+    while (std::isnan(y0))
     {
 
         N_STEP *= 2;
