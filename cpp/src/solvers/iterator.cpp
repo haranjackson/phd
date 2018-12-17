616038091b892981c9ca235c0672a9b9de42d448
Haran Jackson <jackson.haran@gmail.com>
2018-12-17 18:07:34 +0000

    saving previous when last grid is nan

--- cpp/src/solvers/iterator.cpp
+++ cpp/src/solvers/iterator.cpp
@@ -78,6 +78,7 @@ std::vector<Vec> iterator(Vecr u, double tf, iVecr nX, aVecr dX, double CFL,
                           bool STIFF, int FLUX, std::vector<Par> &MPs,
                           double contorted_tol) {
 
+  Vec uprev(u.size());
   std::vector<Vec> ret(100);
   int nmat = MPs.size();
   Vec ub(extended_dimensions(nX, N) * V);
@@ -98,6 +99,8 @@ std::vector<Vec> iterator(Vecr u, double tf, iVecr nX, aVecr dX, double CFL,
   double dt = 0.;
 
   while (t < tf) {
+
+    uprev = u;
 
     if (LSET > 0)
       fill_ghost_cells(grids, masks, u, nX, dX, dt, MPs);
@@ -142,7 +145,8 @@ std::vector<Vec> iterator(Vecr u, double tf, iVecr nX, aVecr dX, double CFL,
     print(dt);
 
     if (u.array().isNaN().any()) {
-      ret[pushCount] = u;
+      ret[pushCount] = uprev;
+      ret[pushCount + 1] = u;
       return ret;
     }
   }
