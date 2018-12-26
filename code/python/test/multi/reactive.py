f272317b4b92def43aef6e73f321407eae442ef4
Haran Jackson <jackson.haran@gmail.com>
2018-12-26 12:23:55 +0000

    confined explosive work

--- code/python/test/multi/reactive.py
+++ code/python/test/multi/reactive.py
@@ -14,85 +14,86 @@ def confined_explosive():
         SPLIT = True
         FLUX = 1
 
-        LSET = 3
+        LSET = 3 / 4
         RIEMANN_STICK = false
         RIEMANN_RELAXATION = true
         STAR_TOL = 1e-8
         PRIM_RECONSTRUCT = true
     """
     BACK_PLATE = False
+    AIR_GAP = False
 
-    Lx = 0.044
-    Ly = 0.1
+    Lx = 0.051
+    Ly = 0.09
 
-    nx = 176
+    nx = 200
     tf = 4.9e-6
+
+    o = 0.0015 if AIR_GAP else 0
 
     ny = int(nx * Ly / Lx)
     dX = [Lx / nx, Ly / ny]
 
     MPm = Steel_SMGP_SI
     MPe = C4_JWL_SI
+    MPa = Air_SG_SI
 
     pm = 1e5
     pe = 1e5
+    pa = 1e5
     v1 = array([700, 0, 0])
     v = zeros(3)
     A = eye(3)
 
-    MPs = [VAC, MPe, MPm, MPm]
+    if AIR_GAP:
+        MPs = [MPa, MPe, MPm, MPm, VAC]
+        u = zeros([nx, ny, 19])
+        u[:, :, -4:] = 1
+    else:
+        MPs = [MPe, MPm, MPm, VAC]
+        u = zeros([nx, ny, 18])
+        u[:, :, -3:] = 1
+
     dX = [Lx / nx, Ly / ny]
 
-    Qm1 = pad(Cvec(MPm.ρ0, pm, v1, MPm, A, λ=0), (0, 3), 'constant')
-    Qm2 = pad(Cvec(MPm.ρ0, pm, v, MPm, A, λ=0), (0, 3), 'constant')
-    Qe = pad(Cvec(MPe.ρ0, pe, v, MPe, A, λ=1), (0, 3), 'constant')
-
-    u = zeros([nx, ny, 18])
+    Qm1 = Cvec(MPm.ρ0, pm, v1, MPm, A, λ=0)
+    Qm2 = Cvec(MPm.ρ0, pm, v, MPm, A, λ=0)
+    Qe = Cvec(MPe.ρ0, pe, v, MPe, A, λ=1)
+    Qa = Cvec(MPa.ρ0, pa, v, MPa, A, λ=0)
 
     for i in range(nx):
         for j in range(ny):
             x = (i+0.5) * dX[0]
             y = (j+0.5) * dX[1]
 
+            # air gap
+            if 0.033 <= x <= 0.033 + o:
+                u[i, j, :15] = Qa
+                u[i, j, -4:] = -1
+
             # projectile
-            if x <= 0.03 and 0.041 <= y <= 0.059:
-                u[i, j] = Qm1
-                u[i, j, -3] = 1
-                u[i, j, -2] = 1
-                u[i, j, -1] = 1
+            elif x <= 0.03 and 0.036 <= y <= 0.054:
+                u[i, j, :15] = Qm1
+                u[i, j, -1] = -1
 
             # front plate
-            elif 0.03 <= x <= 0.03318:
-                u[i, j] = Qm2
-                u[i, j, -3] = 1
-                u[i, j, -2] = 1
-                u[i, j, -1] = -1
+            elif 0.03 <= x <= 0.033:
+                u[i, j, :15] = Qm2
+                u[i, j, -2:] = -1
 
             # explosive
-            elif 0.03318 <= x <= 0.03953:
-                u[i, j] = Qe
-                u[i, j, -3] = 1
-                u[i, j, -2] = -1
-                u[i, j, -1] = -1
+            elif 0.033 + o <= x <= 0.039 + o:
+                u[i, j, :15] = Qe
+                u[i, j, -3:] = -1
 
             # back plate
-            elif 0.03953 <= x:
+            elif 0.039 + o <= x:
                 if BACK_PLATE:
-                    u[i, j] = Qm2
-                    u[i, j, -3] = 1
-                    u[i, j, -2] = 1
-                    u[i, j, -1] = -1
+                    u[i, j, :15] = Qm2
+                    u[i, j, -2:] = -1
                 else:
-                    u[i, j] = Qe
-                    u[i, j, -3] = 1
-                    u[i, j, -2] = -1
-                    u[i, j, -1] = -1
-
-            # vacuum
-            else:
-                u[i, j, -3] = -1
-                u[i, j, -2] = -1
-                u[i, j, -1] = -1
+                    u[i, j, :15] = Qe
+                    u[i, j, -3:] = -1
 
     #u = u[:, int(ny/2):]
 
@@ -111,11 +112,12 @@ def rod_impact():
         STAR_TOL = 1e-8
         PRIM_RECONSTRUCT = true
     """
-    Lx = 0.552
+    Lx = 0.55
     Ly = 0.18
-    nx = 300
-    tf = 45e-6
+    nx = 2000
+    tf = 60e-6
 
+    nx = int(nx)
     ny = int(nx * Ly / Lx)
     dX = [Lx / nx, Ly / ny]
 
@@ -153,9 +155,9 @@ def rod_impact():
                 u[i, j, -1] = 1
 
             # casing
-            elif ((0.06 <= x <= 0.08 or 0.532 <= x <= 0.552) and
+            elif ((0.06 <= x <= 0.08 or 0.53 <= x <= 0.55) and
                   0.02 <= y <= 0.16) or \
-                  (0.08 <= x <= 0.532 and
+                  (0.08 <= x <= 0.53 and
                    (0.02 <= y <= 0.04 or 0.14 <= y <= 0.16)):
                 u[i, j] = Qm2
                 u[i, j, -3] = 1
@@ -163,7 +165,7 @@ def rod_impact():
                 u[i, j, -1] = -1
 
             # explosive
-            elif 0.08 <= x <= 0.532 and 0.04 <= y <= 0.14:
+            elif 0.08 <= x <= 0.53 and 0.04 <= y <= 0.14:
                 u[i, j] = Qe
                 u[i, j, -3] = 1
                 u[i, j, -2] = -1
--- code/python/test/params/fluids.py
+++ code/python/test/params/fluids.py
@@ -3,8 +3,8 @@ from gpr.misc.objects import material_params
 
 """ SI Units """
 
-Air_SG_SI = material_params(EOS='sg', ρ0=1.18, cv=721, γ=1.4,
-                            b0=1, cα=1, μ=1.85e-5, Pr=0.714)
+Air_SG_SI = material_params(EOS='sg', ρ0=1.18, cv=718, γ=1.4,
+                            b0=50, cα=50, μ=1.85e-5, Pr=0.714)
 
 He_SG_SI = material_params(EOS='sg', ρ0=0.163, cv=3127, γ=5/3,
                            b0=1, cα=1, μ=1.99e-5, Pr=0.688)
